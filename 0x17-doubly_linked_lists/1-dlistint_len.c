d3fe8a6fee614eadee1b9d4741d8430461c681a8
Baudile99 <honorekbb@gmail.com>
2022-06-09 16:04:24 -0700

    1-dlistint_len.c add

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,13 +6,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
-	if (!h)
-		return (0);
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
+size_t nodes = 0;
+if (!h)
+return (0);
+while (h)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
